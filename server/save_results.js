77940b79a8834120fedae612fb247bc7e97e753a
sol <solflo@pm.me>
2026-01-22 12:53:04 -0300

    remove more aws

--- server/save_results.js
+++ server/save_results.js
@@ -1,10 +1,10 @@
 const crypto = require('crypto')
 const bucket = 'ganbreederpublic'
-const AWS = require('aws-sdk')
+// const AWS = require('aws-sdk')
 const secrets = require('./secrets.js')
 const knex = require('knex')(secrets.database)
-AWS.config.update(secrets.aws)
-const s3 = new AWS.S3()
+// AWS.config.update(secrets.aws)
+// const s3 = new AWS.S3()
 const fs = require('fs');
 
 const randomString = (n) => crypto.randomBytes(n).toString('hex')
@@ -20,25 +20,29 @@ module.exports = async function({ imgs, vectors, labels, size, parent1=null, par
         })
     }
     const ids = await knex('image').insert(insert).returning('id')
-    if (secrets.local_images) {
-        for (let i = 0; i < imgs.length; i++) {
+    // if (secrets.local_images) {
+    //     for (let i = 0; i < imgs.length; i++) {
+    //         const buf = new Buffer(imgs[i].replace(/^data:image\/\w+;base64,/, ""),'base64')
+    //         fs.writeFileSync("public/img/"+insert[i].key+".jpeg", buf);
+    //     }
+    // } else {
+    //     const uploads = []
+    //     for (let i = 0; i < imgs.length; i++) {
+    //         const buf = new Buffer(imgs[i].replace(/^data:image\/\w+;base64,/, ""),'base64')
+    //         uploads.push(s3.upload({
+    //             Bucket: bucket,
+    //             Key: `imgs/${insert[i].key}.jpeg`,
+    //             Body: buf,
+    //             ACL: 'public-read',
+    //             ContentEncoding: 'base64',
+    //             ContentType: 'image/jpeg'
+    //         }).promise())
+    //     }
+    //     await Promise.all(uploads)
+    // }
+    for (let i = 0; i < imgs.length; i++) {
             const buf = new Buffer(imgs[i].replace(/^data:image\/\w+;base64,/, ""),'base64')
             fs.writeFileSync("public/img/"+insert[i].key+".jpeg", buf);
-        }
-    } else {
-        const uploads = []
-        for (let i = 0; i < imgs.length; i++) {
-            const buf = new Buffer(imgs[i].replace(/^data:image\/\w+;base64,/, ""),'base64')
-            uploads.push(s3.upload({
-                Bucket: bucket,
-                Key: `imgs/${insert[i].key}.jpeg`,
-                Body: buf,
-                ACL: 'public-read',
-                ContentEncoding: 'base64',
-                ContentType: 'image/jpeg'
-            }).promise())
-        }
-        await Promise.all(uploads)
     }
     return insert.map(({ key }) => ({ key }))
 }
